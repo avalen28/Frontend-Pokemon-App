ef1423d13660c904bb8e572e76dde4f2dc32c8bc
Alberto Valenzuela <avalen28@gmail.com>
2023-10-04 16:46:48 +0200

    Adds axios to API connexion. Implements call and the response come with first generation (151 pokemon)

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import PokemonCard from './PokemonCard.jsx'
+import React, {useState,useEffect} from 'react';
+import PokemonCard from './PokemonCard.jsx';
+import axios from 'axios';
 
 const MainPage = () => {
+    const [allPokemons, setAllPokemons] = useState(null);
+    const getPokemonFromApi = async () => {
+        try {
+            const responseFromTheApi = await axios.get(
+              "https://pokeapi.co/api/v2/pokemon/?limit=151"
+            );
+            setAllPokemons(responseFromTheApi.data.results);
+        } catch (error) {
+            console.error("this is the error", error);
+        }
+    };
+    useEffect (() => {
+        getPokemonFromApi();
+    },[]);
     return (
-        <div>
-            <p>Hello! this is Main Page</p>
+      <div>
+        {allPokemons && (
+          <div>
+            <p>IMMFLY</p>
+            <h2>Pokemon</h2>
+            <p>Generation: 1</p>
+            <p>{allPokemons.length} pokemon</p>
             <PokemonCard />
-        </div>
+          </div>
+        )}
+      </div>
     );
 };
 
