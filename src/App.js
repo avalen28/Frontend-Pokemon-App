36c6fc9820de969fa245fb77993c79c277e7ab89
Alberto Valenzuela <avalen28@gmail.com>
2023-10-04 19:54:28 +0200

    Adds Favorites component and route

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import MainPage from './views/MainPage.jsx'
 import PokemonDetail from "./views/PokemonDetail.jsx";
 import './App.css';
+import Favorites from './views/Favorites.jsx';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/pokemon' element={<MainPage/>} />
-        <Route path='/pokemon/:name' element={<PokemonDetail/>}/>
-        <Route path='*' element={<Navigate to="/pokemon" />} />
+        <Route path="/pokemon" element={<MainPage />} />
+        <Route path="/pokemon/favorites" element={<Favorites />} />
+        <Route path="/pokemon/:name" element={<PokemonDetail />} />
+        <Route path="*" element={<Navigate to="/pokemon" />} />
       </Routes>
     </div>
   );
