3feb5fdc32686f95c234caa07ae5ceb8df08b9b0
Alberto Valenzuela <avalen28@gmail.com>
2023-10-06 09:55:41 +0200

    Implements dinamic Add pokemon / remove pokemon from fav.

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { firstUpperCase } from "../utils/functions";
 import PropTypes from "prop-types";
@@ -6,7 +6,22 @@ import PropTypes from "prop-types";
 const PokemonCard = ({ pokemon, showFavButton }) => {
   const name = pokemon;
 
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const getFavPokemonsToChangeFavOption = () => {
+    const currentPokemonsFav = localStorage.getItem("favPokemons");
+
+    let pokemonArr;
+
+    if (currentPokemonsFav !== null) {
+      pokemonArr = currentPokemonsFav.split(",");
+      const result = pokemonArr.filter((pokemon) => pokemon === name);
+   result.length > 0 ? setIsFavorite(true) : setIsFavorite(false)
+    }
+  };
+
   const addPokemonToFav = () => {
+    setIsFavorite(!isFavorite)
     // this is what currently have on the local storage.
     const currentPokemonsFav = localStorage.getItem("favPokemons");
 
@@ -19,17 +34,26 @@ const PokemonCard = ({ pokemon, showFavButton }) => {
     }
 
     if (pokemonArr.includes(name)) {
+      //removes pokemon from fav.
       pokemonArr = pokemonArr.filter((elem) => elem !== name);
     } else {
+      //adds pokemon to fav.
       pokemonArr.push(name);
     }
     localStorage.setItem("favPokemons", pokemonArr);
   };
-
+  useEffect(() => {
+    getFavPokemonsToChangeFavOption()
+  }, []);
   return (
     <div>
-      {showFavButton && <button onClick={addPokemonToFav}>Add to Fav</button>}
+      {showFavButton && isFavorite === false && (
+        <button onClick={addPokemonToFav}>Add to Fav</button>
+      )}
       <br />
+      {showFavButton && isFavorite === true && (
+        <button onClick={addPokemonToFav}>removes</button>
+      )}
       <Link to={`/pokemon/${name}`}>
         <img
           src={`https://img.pokemondb.net/sprites/black-white/anim/normal/${name}.gif`}
